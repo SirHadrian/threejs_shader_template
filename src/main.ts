a58213af967de8ebe0e89898a2cfb787edd54221
Toma Adrian Ionut <sirhadrian21@gmail.com>
2023-04-02 16:12:34 +0300

    basic shaders

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,18 @@ import _FS from './shaders/fragment_shader.c?raw'
 import _VS from './shaders/vertex_shader.c?raw'
 
 
+const uniforms = {
+  R: new THREE.Uniform(new THREE.Vector2(window.innerWidth, window.innerHeight)),
+  T: { value: 1.0 },
+}
+
+const clock = new THREE.Clock()
+
 class SceneSetup extends THREE.Scene {
 
   constructor() {
 
-    super();
+    super()
 
   }
 
@@ -19,10 +26,10 @@ class CameraSetup extends THREE.PerspectiveCamera {
 
   constructor(fov: number, aspectRatio: number, nearDistance: number, farDistance: number) {
 
-    super(fov, aspectRatio, nearDistance, farDistance);
+    super(fov, aspectRatio, nearDistance, farDistance)
 
-    this.position.set(0, 1, 20);
-    this.lookAt(0, 0, 0);
+    this.position.set(0, 1, 20)
+    this.lookAt(0, 0, 0)
   }
 }
 
@@ -30,18 +37,18 @@ class RendererSetup extends THREE.WebGLRenderer {
 
   constructor(configs: object, camera: THREE.Camera) {
 
-    super(configs);
+    super(configs)
 
-    this.setSize(window.innerWidth, window.innerHeight);
-    this.setPixelRatio(window.devicePixelRatio);
-    this.outputEncoding = THREE.sRGBEncoding;
+    this.setSize(window.innerWidth, window.innerHeight)
+    this.setPixelRatio(window.devicePixelRatio)
+    this.outputEncoding = THREE.sRGBEncoding
 
     // Inject renderer to DOM
-    const target = document.getElementById("app");
-    target?.appendChild(this.domElement);
+    const target = document.getElementById("app")
+    target?.appendChild(this.domElement)
 
     // OrbitControls
-    new OrbitControls(camera, this.domElement);
+    new OrbitControls(camera, this.domElement)
   }
 }
 
@@ -49,10 +56,10 @@ class LightSetup extends THREE.DirectionalLight {
 
   constructor(scene: THREE.Scene, color: THREE.ColorRepresentation, intensity: number) {
 
-    super(color, intensity);
+    super(color, intensity)
 
-    this.position.set(0, 10, 10);
-    scene.add(this);
+    this.position.set(0, 10, 10)
+    scene.add(this)
   }
 }
 
@@ -60,7 +67,7 @@ function main() {
 
   //#region INIT
   // Create Scene
-  const scene = new SceneSetup();
+  const scene = new SceneSetup()
 
   // Create Camera
   const camera = new CameraSetup(
@@ -68,36 +75,36 @@ function main() {
     window.innerWidth / window.innerHeight, // Aspect ratio
     0.1, // Near: distance objects apear on camera
     1000, // Far: distance objects disapear from camera
-  );
+  )
 
   // Create Renderer
-  const renderer = new RendererSetup({ antialiasing: true }, camera);
+  const renderer = new RendererSetup({ antialiasing: true }, camera)
   renderer.shadowMap.enabled = true
   // Create light source
   const light = new LightSetup(
     scene,
     0xffffff,
     1
-  );
+  )
 
-  scene.add(light);
+  scene.add(light)
   //#endregion
 
 
   //#region PlayGround
 
 
-  const g = new THREE.PlaneGeometry(10, 10);
+  const g = new THREE.PlaneGeometry(10, 10)
   const m = new THREE.ShaderMaterial({
     side: THREE.DoubleSide,
     vertexShader: _VS,
     fragmentShader: _FS,
-  });
-  const plane = new THREE.Mesh(g, m);
+  })
+  const plane = new THREE.Mesh(g, m)
 
 
 
-  scene.add(plane);
+  scene.add(plane)
 
 
   //#endregion
@@ -107,22 +114,23 @@ function main() {
 
   // On window resize
   const resize = () => {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
   }
-  window.addEventListener("resize", resize, false);
+  window.addEventListener("resize", resize, false)
 
   // Animation loop
   const animate = () => {
 
+    uniforms.T.value = clock.getElapsedTime()
 
-    renderer.render(scene, camera);
-    requestAnimationFrame(animate);
+    renderer.render(scene, camera)
+    requestAnimationFrame(animate)
   }
-  animate();
+  animate()
 
   //#endregion
 }
 
-main();
+main()
--- src/shaders/fragment_shader.c
+++ src/shaders/fragment_shader.c
@@ -1,10 +1,20 @@
 varying vec2 vertexUV;
 
+uniform float T;
+uniform vec2 R;
+
+#define FC gl_FragCoord
+
+#define PI 3.14159265359
+#define S(a, b, x) smoothstep(a, b, x)
+
 void main() {
+
+  vec2 uv = vertexUV - .5;
 
   vec3 color = vec3(0.);
 
-  color.rg = vertexUV;
+  color.rg = uv;
 
   gl_FragColor = vec4(color, 1.);
 }
