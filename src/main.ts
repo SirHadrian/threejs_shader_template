075af741948e17702c8342b5fc998adc49524a80
Toma Adrian <sirhadrian21@gmail.com>
2023-12-04 15:55:31 +0200

    update

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import * as THREE from 'three'
+import Stats from 'stats.js'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 import _FS from './shaders/fragment_shader.c?raw'
@@ -6,131 +7,144 @@ import _VS from './shaders/vertex_shader.c?raw'
 
 
 const uniforms = {
-    R: new THREE.Uniform(new THREE.Vector2(window.innerWidth, window.innerHeight)),
-    T: { value: 1.0 },
+  R: new THREE.Uniform(new THREE.Vector2(window.innerWidth, window.innerHeight)),
+  T: { value: 1.0 },
 }
 
 const clock = new THREE.Clock()
 
 class SceneSetup extends THREE.Scene {
 
-    constructor() {
+  constructor() {
 
-        super()
+    super()
 
-    }
+  }
 
 }
 
 class CameraSetup extends THREE.PerspectiveCamera {
 
-    constructor(fov: number, aspectRatio: number, nearDistance: number, farDistance: number) {
+  constructor(fov: number, aspectRatio: number, nearDistance: number, farDistance: number) {
 
-        super(fov, aspectRatio, nearDistance, farDistance)
+    super(fov, aspectRatio, nearDistance, farDistance)
 
-        this.position.set(0, 1, 20)
-        this.lookAt(0, 0, 0)
-    }
+    this.position.set(0, 1, 20)
+    this.lookAt(0, 0, 0)
+  }
 }
 
 class RendererSetup extends THREE.WebGLRenderer {
 
-    constructor(configs: object, camera: THREE.Camera) {
+  constructor(configs: object, camera: THREE.Camera) {
 
-        super(configs)
+    super(configs)
 
-        this.setSize(window.innerWidth, window.innerHeight)
-        this.setPixelRatio(window.devicePixelRatio)
-        this.outputEncoding = THREE.sRGBEncoding
+    this.setSize(window.innerWidth, window.innerHeight)
+    this.setPixelRatio(window.devicePixelRatio)
+    this.outputEncoding = THREE.sRGBEncoding
 
-        // Inject renderer to DOM
-        const target = document.getElementById("app")
-        target?.appendChild(this.domElement)
+    // Inject renderer to DOM
+    const target = document.getElementById("app")
+    target?.appendChild(this.domElement)
 
-        // OrbitControls
-        new OrbitControls(camera, this.domElement)
-    }
+    // OrbitControls
+    new OrbitControls(camera, this.domElement)
+  }
 }
 
 class LightSetup extends THREE.DirectionalLight {
 
-    constructor(scene: THREE.Scene, color: THREE.ColorRepresentation, intensity: number) {
+  constructor(scene: THREE.Scene, color: THREE.ColorRepresentation, intensity: number) {
 
-        super(color, intensity)
+    super(color, intensity)
 
-        this.position.set(0, 10, 10)
-        scene.add(this)
-    }
+    this.position.set(0, 10, 10)
+    scene.add(this)
+  }
+}
+
+function initStats() {
+  var stats = new Stats();
+  stats.setMode(0);
+  stats.domElement.style.position = 'absolute';
+  stats.domElement.style.left = '0px';
+  stats.domElement.style.top = '0px';
+  document.getElementById("Stats-output")?.appendChild( stats.domElement );
+  return stats;
 }
 
 function main() {
 
-    //#region INIT
-    // Create Scene
-    const scene = new SceneSetup()
+  let stats = initStats()
 
-    // Create Camera
-    const camera = new CameraSetup(
-        50, // FOV
-        window.innerWidth / window.innerHeight, // Aspect ratio
-        0.1, // Near: distance objects apear on camera
-        1000, // Far: distance objects disapear from camera
-    )
+  //#region INIT
+  // Create Scene
+  const scene = new SceneSetup()
 
-    // Create Renderer
-    const renderer = new RendererSetup({ antialiasing: true }, camera)
-    renderer.shadowMap.enabled = true
-    // Create light source
-    const light = new LightSetup(
-        scene,
-        0xffffff,
-        1
-    )
+  // Create Camera
+  const camera = new CameraSetup(
+    50, // FOV
+    window.innerWidth / window.innerHeight, // Aspect ratio
+    0.1, // Near: distance objects apear on camera
+    1000, // Far: distance objects disapear from camera
+  )
 
-    scene.add(light)
-    //#endregion
+  // Create Renderer
+  const renderer = new RendererSetup({ antialiasing: true }, camera)
+  renderer.shadowMap.enabled = true
+  // Create light source
+  const light = new LightSetup(
+    scene,
+    0xffffff,
+    1
+  )
 
-
-    //#region PlayGround
-
-
-    const g = new THREE.PlaneGeometry(10, 10)
-    const m = new THREE.ShaderMaterial({
-        side: THREE.DoubleSide,
-        vertexShader: _VS,
-        fragmentShader: _FS,
-    })
-    const plane = new THREE.Mesh(g, m)
+  scene.add(light)
+  //#endregion
 
 
-
-    scene.add(plane)
-
-
-    //#endregion
+  //#region PlayGround
 
 
-    //#region Main loop and events
+  const g = new THREE.PlaneGeometry(10, 10)
+  const m = new THREE.ShaderMaterial({
+    side: THREE.DoubleSide,
+    vertexShader: _VS,
+    fragmentShader: _FS,
+  })
+  const plane = new THREE.Mesh(g, m)
 
-    // On window resize
-    const resize = () => {
-        camera.aspect = window.innerWidth / window.innerHeight
-        camera.updateProjectionMatrix()
-        renderer.setSize(window.innerWidth, window.innerHeight)
-    }
-    window.addEventListener("resize", resize, false)
 
-    // Animation loop
-    const animate = () => {
 
-        uniforms.T.value = clock.getElapsedTime()
+  scene.add(plane)
 
-        renderer.render(scene, camera)
-        requestAnimationFrame(animate)
-    }
-    animate()
 
-    //#endregion
+  //#endregion
+
+
+  //#region Main loop and events
+
+  // On window resize
+  const resize = () => {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+  }
+  window.addEventListener("resize", resize, false)
+
+  // Animation loop
+  const animate = () => {
+    stats.update()
+
+    uniforms.T.value = clock.getElapsedTime()
+
+    renderer.render(scene, camera)
+    requestAnimationFrame(animate)
+  }
+  animate()
+
+  //#endregion
 }
 
 main()
